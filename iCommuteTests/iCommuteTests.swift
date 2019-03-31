05a2f68f9b900f9ebba82d5235dc2fd650fe806d
William A Rodriguez <wrod@mit.edu>
2019-03-31 10:53:28 -0400

    Initial Commit

---
+++ iCommuteTests/iCommuteTests.swift
@@ -0,0 +1,34 @@
+//
+//  iCommuteTests.swift
+//  iCommuteTests
+//
+//  Created by William A. Rodriguez on 3/31/19.
+//  Copyright © 2019 GoldenRod. All rights reserved.
+//
+
+import XCTest
+@testable import iCommute
+
+class iCommuteTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
