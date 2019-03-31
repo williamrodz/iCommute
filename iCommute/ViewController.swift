1ab36a67777398c06a0de0c3a2326848153fbe17
William A Rodriguez <wrod@mit.edu>
2019-03-31 11:15:39 -0400

    Added basic UI to storyboard

--- iCommute/ViewController.swift
+++ iCommute/ViewController.swift
@@ -5,7 +5,11 @@
 //  Created by William A. Rodriguez on 3/31/19.
 //  Copyright © 2019 GoldenRod. All rights reserved.
 //
-
+// Goals
+//
+// 1) UI : Origin Address, Destination Address
+///2) Google Maps Query for time, distance
+// 3) Notification Center and Settings
 import UIKit
 
 class ViewController: UIViewController {
