7addbe2f6e6129330270e3bf477e22290beb03bb
William A Rodriguez <wrod@mit.edu>
2019-04-04 18:47:01 -0400

    Succesfully managed to convert JSON response into Swift Struct

--- iCommute/MainViewController.swift
+++ iCommute/MainViewController.swift
@@ -7,8 +7,7 @@
 //
 
 import UIKit
-
-
+import UserNotifications
 
 class MainViewController: UIViewController {
     
@@ -59,7 +58,7 @@ class MainViewController: UIViewController {
     
     
     func fetchData(completion: @escaping ([String:Any]?, Error?) -> Void) {
-        //let url = URL(string: "http://api.geekdo.com/api/images?ajax=1&gallery=all&nosession=1&objectid=127023&objecttype=thing&pageid=357&showcount=1&size=thumb&sort=recent")!
+
         let url = getRequestURL(origin: "test", destination: "test")!;
         
         let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
@@ -75,43 +74,149 @@ class MainViewController: UIViewController {
         }
         task.resume()
     }
-    
+	/*
+	 The following struct definitions are needed to decode JSON response
+	*/
+	
+	struct Distance: Codable {
+		let text: String
+		let value: Int
+	}
+	
+	struct Duration_in_traffic: Codable {
+		let text: String
+		let value: Int
+	}
+	struct Duration: Codable {
+		let text: String
+		let value: Int
+	}
+	struct Elements: Codable {
+		let distance: Distance
+		let duration: Duration
+		let duration_in_traffic: Duration_in_traffic
+		let status: String
+	}
+	
+	struct Response: Codable {
+		let destination_addresses: [String]
+		let origin_addresses: [String]
+		let rows: [Rows]
+		let status: String
+	}
+	
+	struct Rows: Codable {
+		let elements: [Elements]
+	}
+	
+	
+	
+	let sampleJSONString = """
+	{
+	"destination_addresses" : [ "77 Massachusetts Ave, Cambridge, MA 02142, USA" ],
+	"origin_addresses" : [ "428 Memorial Dr, Cambridge, MA 02139, USA" ],
+	"rows" : [
+	{
+	"elements" : [
+	{
+	"distance" : {
+	"text" : "1.8 km",
+	"value" : 1754
+	},
+	"duration" : {
+	"text" : "5 mins",
+	"value" : 271
+	},
+	"duration_in_traffic" : {
+	"text" : "4 mins",
+	"value" : 257
+	},
+	"status" : "OK"
+	}
+	]
+	}
+	],
+	"status" : "OK"
+	}
+	"""
+	
+	func decodeJSONString(jsonString:String){
+		let jsonData = jsonString.data(using: .utf8)!
+		let jsonDecoder = JSONDecoder()
+		do {
+			let response1 = try jsonDecoder.decode(Response.self, from: jsonData)
+			let firstRow = response1.rows[0]
+			let firstElement = firstRow.elements[0]
+			let durationStruct = firstElement.duration
+			let durationString = durationStruct.text
+		} catch{
+			print(error)
+		}
 
-    
-    
-    
-    
+	}
 
+	
+	
+
+	func fetchMapsData(){
+		fetchData { (dict, error) in
+			//debugPrint(dict)
+			if let rows = dict?["rows"] as? [[String:Any]]{
+				print("rows is")
+				print(rows)
+					
+				}
+			}
+		}
     
     
     @IBAction func calculateCommuteTimeButton(_ sender: UIButton) {
-        print("hi")
-        fetchData { (dict, error) in
-            //debugPrint(dict)
-            if let rows = dict?["rows"] as? [[String:Any]]{
-                if let elements = rows[0] as? [String:Any]{
-                    print("elements is")
-                    print(elements)
-                    if let actualElements = elements["elements"] as? [String:Any]{
-                        print(actualElements)
-                    }
-                
-                }
-            }
-        }
+		decodeJSONString(jsonString: sampleJSONString);
+		//fetchMapsData();
+		//registerLocal();
+		//scheduleLocal();
+
     }
     
     func saveNewNotification(){
-        var newNotification = CommuteNotification(origin: originField.text ?? testOrigin,destination: destinationField.text ?? testDestination,departureTime: "3:00PM");
+        let newNotification = CommuteNotification(origin: originField.text ?? testOrigin,destination: destinationField.text ?? testDestination,departureTime: "3:00PM");
         savedNotifications.append(newNotification);
     }
-    
-    func sendNotification(){
-        let notificatioName = Notification.Name("hi");
-        let newNotification = Notification(name:notificatioName);
-        
-    }
-    
+	
+	func registerLocal() {
+		let center = UNUserNotificationCenter.current()
+		
+		center.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+			if granted {
+				print("Yay!")
+			} else {
+				print("D'oh")
+			}
+		}
+	}
+	
+	func scheduleLocal() {
+		
+		let center = UNUserNotificationCenter.current()
+		center.removeAllPendingNotificationRequests()
+		
+		let content = UNMutableNotificationContent()
+		content.title = "Late wake up call"
+		content.body = "The early bird catches the worm, but the second mouse gets the cheese."
+		content.categoryIdentifier = "alarm"
+		content.userInfo = ["customData": "fizzbuzz"]
+		content.sound = UNNotificationSound.default
+		
+		var dateComponents = DateComponents()
+		dateComponents.hour = 12
+		dateComponents.minute = 11
+		//let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+		let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+
+		
+		let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+		center.add(request)
+	}
     /*
     // MARK: - Navigation
 
